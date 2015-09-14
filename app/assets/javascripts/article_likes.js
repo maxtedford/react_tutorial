7c47c73411d2f55b344c3d85ff4479c4efb844df
Max Tedford <max.tedford@gmail.com>
2015-09-14 11:13:19 -0600

    add function to update state when clicked

--- app/assets/javascripts/article_likes.js
+++ app/assets/javascripts/article_likes.js
@@ -1,13 +1,17 @@
 var likeArticle = React.createClass({
   render: function() {
+    console.log(this.state.isLiked);
     if(this.state.isLiked){
-      return React.createElement("div", {}, "Unlike me!");
+      return React.createElement("div", { onClick: this.clickHandler }, "Unlike me!");
     } else {
-      return React.createElement("div", {}, "Like me!");
+      return React.createElement("div", { onClick: this.clickHandler }, "Like me!");
     }
   },
   getInitialState: function() {
     return {isLiked: false}
+  },
+  clickHandler: function() {
+    this.setState({ isLiked: !this.state.isLiked })
   }
 });
 
