a58233f94c086b086979f77489ac76ca63491f3d
Max Tedford <max.tedford@gmail.com>
2015-09-14 10:30:17 -0600

    update the functions to log Hello Max

--- app/assets/javascripts/article_likes.js
+++ app/assets/javascripts/article_likes.js
@@ -1,12 +1,13 @@
 var Hello = React.createClass({
   render: function() {
-    return React.createElement("div", null, "Hello World!")
+    console.log(this.props);
+    return React.createElement("div", null, "Hello " + this.props.name + "!")
   }
 });
 
 $(document).ready(function () {
   React.render(
-    React.createElement(Hello),
+    React.createElement(Hello, {name: "Max"}),
     document.getElementById("articles")
   )
 });
